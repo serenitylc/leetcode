a2452dca595bca334301c42da0d81903669cc1cf
LiYiShen <1757921819@qq.com>
2019-10-30 14:12:43 +0800

    one to one by name

---
+++ 09isPalindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            y = str(x)[::-1]
+            if y==str(x):
+                return True
+            else:
+                return False
---
+++ 13romanToInt.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        map = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        sum = 0
+        for i in range(len(s)):
+            if i<len(s)-1 and map[s[i]] < map[s[i+1]]:
+                sum -= map[s[i]]
+            else:
+                sum += map[s[i]]
+        return sum
