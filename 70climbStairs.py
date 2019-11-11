b9d0c46dd8f843429f20dc160e40e52838799c55
LiYiShen <1757921819@qq.com>
2019-11-11 23:08:33 +0800

    70

---
+++ 70climbStairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        i1 = 1
+        i2 = 2
+        if n==1:
+            return i1
+        for i in range(3, n+1):
+            temp = i1 + i2
+            i1 = i2
+            i2 = temp
+        return i2
+        
+# dp[i] = dp[i-1] + dp[i-2]: 第i次上楼梯方法数 = 第i-1次上楼梯方法数（再走一步） + 第i-2次上楼梯方法数（再走两步）
---
+++ Leetcode_with_Cpp/70.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2)
+            return n;
+        int num1 = 1;
+        int num2 = 2;
+        int numN = 0;
+        for(int i = 2; i < n; i++){
+            numN = num1 + num2;
+            num1 = num2;
+            num2 = numN;
+        }
+        return numN;
+    }
+};
