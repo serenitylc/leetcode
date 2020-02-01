2719b226a4aec18a52244a4bddfe90222b974bc2
LiYiShen <1757921819@qq.com>
2020-02-01 23:57:01 +0800

    226

---
+++ Leetcode_with_Cpp/226.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr)
+            return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* head = q.front();
+            q.pop();
+            TreeNode* tmp = head->left;
+            head->left = head->right;
+            head->right = tmp;
+            if(head->left != nullptr)
+                q.push(head->left);
+            if(head->right != nullptr)
+                q.push(head->right);
+        }
+        return root;
+    }
+};
