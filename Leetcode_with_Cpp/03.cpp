905d11f1a2ce58a1eeb4412286a4b7b8a07f7e23
LiYiShen <1757921819@qq.com>
2020-02-14 16:16:24 +0800

    03

--- Leetcode_with_Cpp/03.cpp
+++ Leetcode_with_Cpp/03.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
+
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int size,i=0,j,k,max=0;
+        int size, i = 0, j, k, max = 0;
         size = s.size();
-        for(j=0; j<size; j++){
-            for(k=i; k<j; k++)
-                if(s[k]==s[j]){
-                    i = k+1;
+        for(j = 0; j < size; j++) {
+            for(k = i; k < j; k++)
+                if(s[k] == s[j]) {
+                    i = k + 1;
                     break;
                 }
             if(j-i+1 > max)
@@ -18,6 +19,7 @@ public:
         return max;
     }
 };
+
 int main()
 {
 	int value;
@@ -25,3 +27,24 @@ int main()
 	value = a.lengthOfLongestSubstring("abcabcbb");
 	cout << "value: " << value << endl;
 }
+
+/*
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int freq[256] = {0};
+        int l = 0, r = -1;  // »¬¶¯´°¿Ú s[l...r]
+        int res = 0;
+        while(l < s.size()) {
+            if(freq[s[r+1]] == 0 && r + 1 < s.size())
+                freq[s[++r]] ++;
+            else
+                freq[s[l++]] --;
+            res = max(res, r-l+1);
+        }
+        return res;
+    }
+};
+
+*/
