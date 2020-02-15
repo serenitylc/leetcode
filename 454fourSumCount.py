dfbe2b44f17212603b1c579a9303fc709795d2d7
LiYiShen <1757921819@qq.com>
2020-02-15 19:11:31 +0800

    454

---
+++ 454fourSumCount.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        count = 0
+        ab_map = dict()
+        for a in A:
+            for b in B:
+                ab_map[a + b] = ab_map.get(a + b, 0) + 1
+            
+        for c in C:
+            for d in D:
+                s = -(c + d)
+                if s in ab_map:
+                    count += ab_map[s]
+        
+        return count
