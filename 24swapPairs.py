9e3442b7c7f6cee21ecdd45f2579611851c7c081
LiYiShen <1757921819@qq.com>
2020-02-17 22:46:22 +0800

    24

---
+++ 24swapPairs.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        else:
+            head, head.next, head.next.next = head.next, head, self.swapPairs(head.next.next)
+            return head
+
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node = head
+        while node and node.next:
+            node.val, node.next.val = node.next.val, node.val
+            node = node.next.next
+        return head
---
+++ Leetcode_with_Cpp/24.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dHead = new ListNode(0);
+        dHead->next = head;
+
+        ListNode* p = dHead;
+        while(p->next && p->next->next) {
+            ListNode* node1 = p->next;
+            ListNode* node2 = node1->next;
+            ListNode* next = node2->next;
+
+            node2->next = node1;
+            node1->next = next;
+            p->next = node2;
+
+            p = node1;
+        }
+
+        ListNode* resNode = dHead->next;
+        delete dHead;
+
+        return resNode;
+    }
+};
