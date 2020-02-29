14b5e6a1944611bcef60159122e0cf44a74a4206
LiYiShen <1757921819@qq.com>
2020-02-29 19:57:42 +0800

    46

---
+++ Leetcode_with_Cpp/46.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    vector<vector<int>> res;
+    vector<bool> used;
+    void generatePermutation(const vector<int>& nums, int index, vector<int>& p) { // ªÿÀ›À„∑®
+        if(index == nums.size()) {
+            res.push_back(p);
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(!used[i]) {
+                p.push_back(nums[i]);
+                used[i] = true;
+                generatePermutation(nums, index + 1, p);
+                p.pop_back();
+                used[i] = false;
+            }
+        }
+        return;
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if(nums.size() == 0)
+            return res;
+        
+        used = vector<bool>(nums.size(), false);
+        vector<int> p;
+        generatePermutation(nums, 0, p);
+        return res;
+    }
+};
