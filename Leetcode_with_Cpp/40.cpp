593d50d87088b496df4ddd3550b37c6de227105f
LiYiShen <1757921819@qq.com>
2020-03-01 15:18:20 +0800

    40

---
+++ Leetcode_with_Cpp/40.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> p;
+        backtrace(candidates, p, 0, target);
+        return res;
+    }
+    void backtrace(vector<int>& candidates, vector<int>& p, int pos, int target) {
+        if(target == 0) {
+            res.push_back(p);
+            return;
+        }
+        for(int i = pos; i < candidates.size(); i++) {
+            if(target - candidates[i] >= 0) {
+                if (i != pos && candidates[i] == candidates[i-1]) // »•µÙ÷ÿ∏¥
+                    continue;
+                p.push_back(candidates[i]);
+                backtrace(candidates, p, i + 1, target-candidates[i]);
+                p.pop_back();
+            }
+            else{ // ºÙ÷¶
+                break;
+            }
+        }
+    }
+};
