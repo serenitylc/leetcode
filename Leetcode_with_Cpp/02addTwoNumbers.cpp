c07f6317adbba52626e48b2aa79a7dc177577e05
LiYiShen <1757921819@qq.com>
2020-05-14 00:23:51 +0800

    02

--- Leetcode_with_Cpp/02addTwoNumbers.cpp
+++ Leetcode_with_Cpp/02addTwoNumbers.cpp
@@ -6,22 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- // flag �����λ 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
        ListNode vHead(0), *p = &vHead;
-        int flag = 0;
+        int flag = 0;  // flag 保存进位 
         while (l1 || l2 || flag) {
             int tmp = 0;
             if (l1 != nullptr) tmp += l1->val;
             if (l2 != nullptr) tmp += l2->val;
-            tmp += flag;     
+            tmp += flag;
+
             flag = tmp / 10;
             tmp %= 10; 
+
+            // 利用已有节点构成新链条 破坏了原链表结构 节约了空间
             ListNode *next = l1 ? l1 : l2;
-            if (next == nullptr) next = new ListNode(tmp);
+            if (next == nullptr) next = new ListNode(tmp); 
+            // ListNode *next = new ListNode(tmp);
             next->val = tmp;
+            
             p->next = next;
             p = p->next;
             l1 = l1 ? l1->next : nullptr;
