56d034ec00433ad3893dc6984abc3df4300d78e1
LiYiShen <1757921819@qq.com>
2020-05-16 17:17:52 +0800

    17

--- Leetcode_with_py/17letterCombinations.py
+++ Leetcode_with_py/17letterCombinations.py
@@ -11,8 +11,7 @@ class Solution(object):
                '6': ['m', 'n', 'o'],
                '7': ['p', 'q', 'r', 's'],
                '8': ['t', 'u', 'v'],
-               '9': ['w', 'x', 'y', 'z']
-        }
+               '9': ['w', 'x', 'y', 'z'] }
         if digits == '':
             return []
         ans = ['']
