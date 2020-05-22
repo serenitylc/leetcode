8db05d4ae43dbc8f7945b168359c70a13b301df1
LiYiShen <1757921819@qq.com>
2020-05-22 12:17:57 +0800

    27

--- Leetcode_with_Cpp/27removeElement.cpp
+++ Leetcode_with_Cpp/27removeElement.cpp
@@ -3,8 +3,8 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int i=0;
         int n = nums.size();
-        for(int j=0; j<n; j++){
-            if(nums[j] != val){
+        for(int j = 0; j < n; j++) {
+            if(nums[j] != val) {
                 nums[i] = nums[j];
                 i++;
             }
