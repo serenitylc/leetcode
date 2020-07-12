160053ecfe212cb23b1c365985c90dc912a8cbb6
LiYiShen <1757921819@qq.com>
2020-07-12 17:18:27 +0800

    35

--- Leetcode_with_Cpp/35searchInsert.cpp
+++ Leetcode_with_Cpp/35searchInsert.cpp
@@ -10,7 +10,6 @@ public:
     }
 };
 
-
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -22,7 +21,6 @@ public:
         
     }
 };
-
 // lower_bound( )和upper_bound( )都是利用二分查找的方法在一个排好序的数组中进行查找的。
 // lower_bound( begin,end,num)：从数组的begin位置到end-1位置二分查找第一个大于或等于num的数字，找到返回该数字的地址，不存在则返回end。
 // 通过返回的地址减去起始地址begin,得到找到数字在数组中的下标。
