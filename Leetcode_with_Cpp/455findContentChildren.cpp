1a63942e861c25ffcb6f4c372bde074b3e2d1a58
LiYiShen <1757921819@qq.com>
2020-08-01 15:52:02 +0800

    455

---
+++ Leetcode_with_Cpp/455findContentChildren.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end(), greater<int>());
+        sort(s.begin(), s.end(), greater<int>());
+        int gi = 0, si = 0;
+        int res = 0;
+        while(gi < g.size() && si < s.size()) {
+            if(s[si] >= g[gi]) {
+                res++;
+                si++;
+                gi++;
+            }else
+                gi++;
+        }
+        return res;
+    }
+};
---
+++ Leetcode_with_py/455findContentChildren.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort(reverse=True)
+        s.sort(reverse=True)
+        gi, si, res =0, 0, 0
+        while gi <len(g) and si < len(s):
+            if s[si] >= g[gi]:
+                res += 1
+                si += 1
+                gi += 1
+            else:
+                gi += 1
+        return res
