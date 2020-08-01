fb0111213c345cc6debe3887b354ded2d62aefa7
LiYiShen <1757921819@qq.com>
2020-08-01 13:29:56 +0800

    53

--- Leetcode_with_Cpp/53maxSubArray.cpp
+++ Leetcode_with_Cpp/53maxSubArray.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        //dp
-        //1.状态定义 F(i): 下标为i时连续数组的最大和
-        //2.状态转移方程 F(i) = max(F(i-1)+nums[i], nums[i]);
-        //3.初始状态 F(0) = nums[0];
-        //4.返回值 F(nums.size()-1)
-
-        if(nums.empty()) 
-            return 0;  
-        vector<int> v(nums);
-        int maxNum = nums[0];
-        for(int i = 1; i < nums.size(); i++)
-        {
-            v[i] = max(v[i-1]+nums[i], nums[i]);
-            if(maxNum < v[i])
-                maxNum = v[i];
+        if(nums.size() == 0)
+            return 0;
+        int res= nums[0];
+        vector<int> dp(nums.size(), res);
+        for(int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            res = max(res, dp[i]);
         }
-        return maxNum;
+        return res;
     }
 };
