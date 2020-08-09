27bfe69837a93d0fe9f5393329a89185537af519
LiYiShen <1757921819@qq.com>
2020-08-09 17:21:59 +0800

    122

--- Leetcode_with_Cpp/122maxProfit.cpp
+++ Leetcode_with_Cpp/122maxProfit.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int p=0;
-        for(int i=1; i<prices.size(); i++)
-            p += max(0,prices[i]-prices[i-1]);
-        return p;
+        int n = prices.size();
+        if(n <= 1)
+            return 0;
+        int dp_i_0 = 0, dp_i_1 = INT_MIN;  // base (i = -1)
+        for(int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
     }
 };
+
+/* k = +infinity:
+dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+            = max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
+
+我们发现数组中的 k 已经不会改变了，也就是说不需要记录 k 这个状态了：
+dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+*/
