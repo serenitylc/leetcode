87ae34a6e924dd313bfb5d9b980954081af8e33a
LiYiShen <1757921819@qq.com>
2020-09-10 19:56:04 +0800

    110

---
+++ Leetcode_with_Cpp/110isBalanced.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
+    }
+    int dfs(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        int left = dfs(root->left);
+        if(left == -1)
+            return -1;
+        int right = dfs(root->right);
+        if(right == -1)
+            return -1;
+        return abs(left - right) > 1 ? -1 : 1 + max(left, right);
+    }
+};
