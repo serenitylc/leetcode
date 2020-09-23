6d5e9359be1729a57bba4810102d1083d2819f18
LiYiShen <1757921819@qq.com>
2020-09-23 16:53:09 +0800

    106

---
+++ Leetcode_with_Cpp/106buildTree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return build(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
+    }
+    TreeNode* build(vector<int>& inorder, int inStart, int inEnd, vector<int>& postorder, int postStart, int postEnd) {
+        if (inStart > inEnd)
+            return NULL;
+        int rootVal = postorder[postEnd];
+        int idx = -1;
+        for (int i = inStart; i <= inEnd; ++i) {
+            if (inorder[i] == rootVal) {
+                idx = i;
+                break;
+            }
+        }
+        int leftSize = idx - inStart;
+        TreeNode* root = new TreeNode(rootVal);
+        root->left = build(inorder, inStart, idx - 1, postorder, postStart, postStart + leftSize - 1);
+        root->right = build(inorder, idx + 1, inEnd, postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+};
