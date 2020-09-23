04e219555a366106f4dd842d0ce81aed6ebdb872
LiYiShen <1757921819@qq.com>
2020-09-23 16:53:20 +0800

    114

---
+++ Leetcode_with_Cpp/114flatten.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (root == nullptr)
+            return;
+        flatten(root->left);
+        flatten(root->right);
+
+        TreeNode *left = root->left;
+        TreeNode *right = root->right;
+        root->left = nullptr;
+        root->right = left;
+        // TreeNode *tmp = root->right;
+        // if (tmp == nullptr) {
+        //     root->right = right;
+        //     return;
+        // }
+        TreeNode *tmp = root;
+        while (tmp->right) {
+            tmp = tmp->right;
+        }
+        tmp->right = right;
+    }
+};
