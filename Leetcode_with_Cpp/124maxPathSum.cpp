9d9a6bba27bf7f10376f56e628b9f0da0f24d858
LiYiShen <1757921819@qq.com>
2020-10-20 13:12:36 +0800

    124

---
+++ Leetcode_with_Cpp/124maxPathSum.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int res = INT_MIN;
+public:
+    int maxPathSum(TreeNode* root) {
+        oneSideMax(root);
+        return res;
+    }
+    int oneSideMax(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = max(0, oneSideMax(root->left));
+        int right = max(0, oneSideMax(root->right));
+        res = max(res, left + right + root->val);
+        return max(left, right) + root->val;
+    }
+};
