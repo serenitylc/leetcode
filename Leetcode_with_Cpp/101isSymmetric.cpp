9d7a0fe8a81c39c01113875380cef510deb582ed
LiYiShen <1757921819@qq.com>
2020-11-02 16:11:05 +0800

    101

--- Leetcode_with_Cpp/101isSymmetric.cpp
+++ Leetcode_with_Cpp/101isSymmetric.cpp
@@ -11,13 +11,13 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if(!root)   return true;
+        if(!root) return true;
         return isMirror(root->left, root->right);
     }
-    bool isMirror(TreeNode* p, TreeNode* q){
-        if(p==NULL && q==NULL)
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
             return true;
-        if((p!=NULL && q==NULL) || (p==NULL && q!=NULL))
+        if((p != NULL && q == NULL) || (p == NULL && q != NULL))
             return false;
         if(p->val == q->val)
             return isMirror(p->left, q->right) && isMirror(p->right, q->left);
