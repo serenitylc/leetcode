54f5152b55d81407799649cd8a4e96fe07f897f2
LiYiShen <1757921819@qq.com>
2020-11-06 15:17:50 +0800

    452

---
+++ Leetcode_with_Cpp/452findMinArrowShots.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.size() == 0) return 0;
+        sort(points.begin(), points.end(), cmp);
+        int res = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.size(); ++i)
+            if (points[i][0] > end) {
+                end = points[i][1];
+                res++;
+            }
+        return res;
+    }
+};
+
+// 区间边界触碰也算重叠
