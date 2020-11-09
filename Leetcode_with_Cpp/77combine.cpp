b0e8e9f232bea2c17731306da13c1d9b6366a1d6
LiYiShen <1757921819@qq.com>
2020-11-09 20:50:05 +0800

    77

--- Leetcode_with_Cpp/77combine.cpp
+++ Leetcode_with_Cpp/77combine.cpp
@@ -1,26 +1,22 @@
 class Solution {
+private:
     vector<vector<int>> res;
-
 public:
     vector<vector<int>> combine(int n, int k) {
-        res.clear();
-        if(n <= 0 || k <= 0 || n < k)
-            return res;
-        vector<int> c;
-        generateCombine(n, k, 1, c);
+        if (n <= 0 || k <= 0 || n < k) return res;
+        vector<int> track;
+        backtrack(n, k, 1, track);
         return res;
     }
-
-    void generateCombine(int n, int k, int start, vector<int>& c) {
-        if(c.size() == k) {
-            res.push_back(c);
+    void backtrack(int n, int k, int start, vector<int>& track) {
+        if (track.size() == k) {
+            res.push_back(track);
             return;
         }
-        for(int i = start; i <= n - (k - c.size()) + 1; i++) {  // 剪枝--减少循环次数
-            c.push_back(i);
-            generateCombine(n, k, i+1, c);
-            c.pop_back();
+        for (int i = start; i <= n - (k - track.size()) + 1; i++) {  // 剪枝--减少循环次数
+            track.push_back(i);
+            backtrack(n, k, i + 1, track);
+            track.pop_back();
         }
-        return;
     }
 };
