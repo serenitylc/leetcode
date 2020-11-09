93c26c8472241d9522f97a3ef50eddf9ff1d8a77
LiYiShen <1757921819@qq.com>
2020-11-09 20:49:57 +0800

    46

--- Leetcode_with_Cpp/46permute.cpp
+++ Leetcode_with_Cpp/46permute.cpp
@@ -1,32 +1,28 @@
 class Solution {
 private:
     vector<vector<int>> res;
-    vector<bool> used;
-    void generatePermutation(const vector<int>& nums, int index, vector<int>& p) { // ªÿÀ›À„∑®
-        if(index == nums.size()) {
-            res.push_back(p);
-            return;
-        }
-        for(int i = 0; i < nums.size(); i++) {
-            if(!used[i]) {
-                p.push_back(nums[i]);
-                used[i] = true;
-                generatePermutation(nums, index + 1, p);
-                p.pop_back();
-                used[i] = false;
-            }
-        }
-        return;
-    }
-
+    vector<bool> visited;
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.size() == 0)
-            return res;
-        
-        used = vector<bool>(nums.size(), false);
-        vector<int> p;
-        generatePermutation(nums, 0, p);
+        if (nums.size() == 0) return res;
+        visited = vector<bool>(nums.size(), false);
+        vector<int> track;
+        backtrack(nums, track);
         return res;
+    }
+    void backtrack(const vector<int>& nums, vector<int>& track) {
+        if (track.size() == nums.size()) {
+            res.push_back(track);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!visited[i]) {
+                track.push_back(nums[i]);
+                visited[i] = true;
+                backtrack(nums, track);
+                track.pop_back();
+                visited[i] = false;
+            }
+        }
     }
 };
