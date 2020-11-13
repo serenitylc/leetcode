7b72b5a8479733643a2bbbb4ce2dadc584575341
LiYiShen <1757921819@qq.com>
2020-11-13 10:57:09 +0800

    03

--- Leetcode_with_Cpp/03lengthOfLongestSubstring.cpp
+++ Leetcode_with_Cpp/03lengthOfLongestSubstring.cpp
@@ -5,8 +5,8 @@ public:
         int freq[256] = {0};
         int l = 0, r = -1;  
         int res = 0;
-        while(l < s.size()) {
-            if(freq[s[r + 1]] == 0 && r + 1 < s.size())
+        while (l < s.size()) {
+            if (freq[s[r + 1]] == 0 && r + 1 < s.size())
                 freq[s[++r]] ++;
             else
                 freq[s[l++]] --;
