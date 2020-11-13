4eded11edf204030d9d16bece8a153bdaa75cfe6
LiYiShen <1757921819@qq.com>
2020-11-13 10:57:18 +0800

    09

--- Leetcode_with_Cpp/09isPalindrome.cpp
+++ Leetcode_with_Cpp/09isPalindrome.cpp
@@ -4,10 +4,9 @@ public:
         if (x < 0) return false;
         else if (x >= 0 && x < 10) return true;
         else if (x % 10 == 0) return false;
-
         int tmp = x;
         unsigned int run = 0;  // 反转可能导致溢出
-        while(tmp != 0) {
+        while (tmp != 0) {
             run = run * 10 + tmp % 10;
             tmp /= 10;
         }
