6948eeba77d7f37116f831a436bca68e9f01bfbf
LiYiShen <1757921819@qq.com>
2020-11-22 17:49:54 +0800

    463

---
+++ Leetcode_with_Cpp/463islandPerimeter.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    res += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) res -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) res -= 2;
+                }
+            }
+        }
+        return res;
+    }
+};
