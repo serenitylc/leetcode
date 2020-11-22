efc9779c234b135b198bd70e546ce0ab32c32464
LiYiShen <1757921819@qq.com>
2020-11-22 17:49:50 +0800

    322

--- Leetcode_with_Cpp/322coinChange.cpp
+++ Leetcode_with_Cpp/322coinChange.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, amount+1);
+        vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-        for(int i = 1; i <= amount; i++)
-            for(int coin : coins) {
-                if(i - coin < 0)
-                    continue;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) continue;
                 dp[i] = min(dp[i], 1 + dp[i - coin]);
             }
-        return (dp[amount] == amount+1) ? -1 : dp[amount];
+        }   
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 };
