9d7ec364443c864f5b1e396a5850c595e744fcf2
LiYiShen <1757921819@qq.com>
2020-11-22 17:50:00 +0800

    509

--- Leetcode_with_Cpp/509fib.cpp
+++ Leetcode_with_Cpp/509fib.cpp
@@ -2,33 +2,36 @@ class Solution {
     vector<int> memo;
 public:
     int fib(int N) {
-        if(N == 0)  return 0;
-        memo = vector<int>(N+1, 0);
+        if (N == 0) return 0;
+        memo = vector<int>(N + 1, 0);
         return helper(N);
     }
     int helper(int n) {
-        if(n == 1 || n == 2)    return 1;
-        if(memo[n] != 0)    return memo[n];
-        memo[n] = helper(n-1) + helper(n-2);
+        if (n == 1 || n == 2) return 1;
+        if (memo[n] != 0) return memo[n];
+        memo[n] = helper(n - 1) + helper(n - 2);
         return memo[n];
     }
 };
+
 class Solution {
 public:
     int fib(int N) {
-        vector<int> dp(31,0);   dp[1]=1;
-        for(int i = 2; i <= N; i++)
-            dp[i] = dp[i-1] + dp[i-2];
+        vector<int> dp(31, 0);
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         return dp[N];
     }
 };
+
 class Solution {
 public:
     int fib(int N) {
-        if(N == 0)  return 0;
-        if (N == 1 || N == 2)   return 1;
+        if (N == 0) return 0;
+        if (N == 1 || N == 2) return 1;
         int prev = 1, curr = 1;
-        for(int i = 3; i <= N; i++) {
+        for (int i = 3; i <= N; i++) {
             int sum = prev + curr;
             prev = curr;
             curr = sum;
