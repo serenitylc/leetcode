52530d4c94f7f8e50da3adfd78fcf7cd96a79e55
LiYiShen <1757921819@qq.com>
2020-11-22 17:49:45 +0800

    111

--- Leetcode_with_Cpp/111minDepth.cpp
+++ Leetcode_with_Cpp/111minDepth.cpp
@@ -10,16 +10,13 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(!root)
-            return 0;
-        if(!root->left && !root->right)
-            return 1;
+        if (!root) return 0;
+        if (!root->left && !root->right) return 1;
         // 根节点的左或右子树为空是构不成子树的, 即null节点不参与比较。
-        if(!root->left && root->right)
+        if (!root->left && root->right)
             return 1 + minDepth(root->right);
-        if(root->left && !root->right)
+        if (root->left && !root->right)
             return 1 + minDepth(root->left);
-
         int leftDepth = minDepth(root->left);
         int rightDepth = minDepth(root->right);
         return min(leftDepth, rightDepth) + 1;
@@ -29,22 +26,18 @@ public:
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(root == nullptr)
-            return 0;
+        if (root == nullptr) return 0;
         queue<TreeNode*> q;
         q.push(root);
         int depth = 1;
-        while(!q.empty()) {
+        while (!q.empty()) {
             int size = q.size();
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode* cur = q.front();
                 q.pop();
-                if(!cur->left && !cur->right)
-                    return depth;
-                if(cur->left)
-                    q.push(cur->left);
-                if(cur->right)
-                    q.push(cur->right);
+                if (!cur->left && !cur->right) return depth;
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
             }
             depth++;
         }
