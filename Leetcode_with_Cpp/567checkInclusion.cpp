480d820e11c97f2f4c5f849f79fadba423da69bd
LiYiShen <1757921819@qq.com>
2020-11-23 16:52:03 +0800

    567

---
+++ Leetcode_with_Cpp/567checkInclusion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> need, window;
+        for (char c : s1) need[c]++;
+        int left = 0, right = 0;
+        int valid = 0;
+        while (right < s2.size()) {
+            char c = s2[right];
+            right++;
+            if (need.count(c)) {
+                window[c]++;
+                if (window[c] == need[c]) valid++;
+            }
+            while (right - left >= s1.size()) {  // 保证滑动窗口大小不超过s1的长度
+                if (valid == need.size()) return true;
+                char d = s2[left];
+                left++;
+                if (need.count(d)) {
+                    if (window[d] == need[d]) valid--;
+                    window[d]--;
+                }
+            }
+        }
+        return false;
+    }
+};
