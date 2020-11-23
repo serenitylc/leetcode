61e141a1604f27cba3897cd3b2e2f2fc482c24e9
LiYiShen <1757921819@qq.com>
2020-11-23 16:52:09 +0800

    704

--- Leetcode_with_Cpp/704search.cpp
+++ Leetcode_with_Cpp/704search.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1;
-        while(left <= right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            }else {
+            } else {
                 right = mid - 1;
             }
         }
