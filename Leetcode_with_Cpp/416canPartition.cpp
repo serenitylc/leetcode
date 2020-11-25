4f531f85b71d65fc894b7fec774fade7fb2bea53
LiYiShen <1757921819@qq.com>
2020-11-25 14:03:11 +0800

    416

--- Leetcode_with_Cpp/416canPartition.cpp
+++ Leetcode_with_Cpp/416canPartition.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int num : nums)    sum += num;
-        if(sum % 2 != 0)    return false;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
         vector<vector<bool>> dp(nums.size(), vector<bool>((sum/=2)+1, false)); // sum 已减半
         // dp[i][j] 考虑前i个数，是否等于j (二维数组的动态规划)
-        for(int i = 0; i < nums.size(); i++)
-            for(int j = 0; j <= sum; j++) {
-                if(i == 0)
+        for (int i = 0; i < nums.size(); i++)
+            for (int j = 0; j <= sum; j++) {
+                if (i == 0)
                     dp[i][j] = (nums[i] == j);
                 else
                     dp[i][j] = dp[i-1][j] || (j-nums[i] >= 0 ? dp[i-1][j-nums[i]] : false);
@@ -20,13 +20,13 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int num : nums)    sum += num;
-        if(sum % 2 != 0)    return false;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
         vector<bool> dp((sum/=2)+1, false); // sum 已减半
         // dp[j] 考虑前i个数，是否等于j (后一行只依赖前一行，转换为一维数组的动态规划)
-        for(int i = 0; i < nums.size(); i++)
-            for(int j = sum; j >= 0; j--)   // i==0 得到初值，然后每次都从后向前遍历修改 dp[j]
-                if(i == 0)
+        for (int i = 0; i < nums.size(); i++)
+            for (int j = sum; j >= 0; j--)   // i==0 得到初值，然后每次都从后向前遍历修改 dp[j]
+                if (i == 0)
                     dp[j] = (nums[i] == j);
                 else
                     dp[j] = dp[j] || (j-nums[i] >= 0 ? dp[j-nums[i]] : false);
@@ -40,8 +40,7 @@ public:
     bool canPartition(vector<int>& nums) {
         n = nums.size();
         int target = accumulate(nums.begin(), nums.end(), 0); 
-        if (target & 1) 
-            return false; 
+        if (target & 1) return false; 
         target >>= 1;
         sort(nums.begin(), nums.end(), [](int& a, int& b) {return a > b; });
         if (nums[0] > target) return false;
@@ -55,7 +54,6 @@ public:
             return true;
         if (nums[idx] > target) 
             return dfs(nums, idx + 1, target);
-
         return dfs(nums, idx + 1, target - nums[idx]) || dfs(nums, idx + 1, target);
     }
 };
