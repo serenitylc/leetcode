3ab4bc57a646a1bcd4ac198aff692749c552c36d
LiYiShen <1757921819@qq.com>
2020-11-25 16:29:14 +0800

    20

--- Leetcode_with_Cpp/20isValid.cpp
+++ Leetcode_with_Cpp/20isValid.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> S;
-        for(int i = 0; i < s.size(); i++) {
-            if(S.size() == 0)   S.push(s[i]);
-            else if(S.top() =='(' && s[i] == ')')   S.pop();
-            else if(S.top() =='{' && s[i] == '}')   S.pop(); 
-            else if(S.top() =='[' && s[i] == ']')   S.pop(); 
-            else S.push(s[i]);
+        stack<int> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (st.size() == 0) st.push(s[i]);
+            else if (st.top() =='(' && s[i] == ')') st.pop();
+            else if (st.top() =='{' && s[i] == '}') st.pop(); 
+            else if (st.top() =='[' && s[i] == ']') st.pop(); 
+            else st.push(s[i]);
         }
-        if(S.size() == 0) 
-            return true;
-        else 
-            return false;   
+        return st.size() == 0; 
     }
 };
