d3f0b9d5f3a4651c5c0d11d2c12e2a41810d5ab2
LiYiShen <1757921819@qq.com>
2020-11-25 14:02:51 +0800

    122

--- Leetcode_with_Cpp/122maxProfit.cpp
+++ Leetcode_with_Cpp/122maxProfit.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if(n <= 1)
-            return 0;
+        if (n <= 1) return 0;
         int dp_i_0 = 0, dp_i_1 = INT_MIN;  // base (i = -1)
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int temp = dp_i_0;
             dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
             dp_i_1 = max(dp_i_1, temp - prices[i]);
@@ -15,11 +14,11 @@ public:
 };
 
 /* k = +infinity:
-dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
-dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
-            = max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
-
-我们发现数组中的 k 已经不会改变了，也就是说不需要记录 k 这个状态了：
-dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
-*/
+ * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+ * dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+ *             = max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
+ *
+ * 我们发现数组中的 k 已经不会改变了，也就是说不需要记录 k 这个状态了：
+ * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+ * dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+ */
