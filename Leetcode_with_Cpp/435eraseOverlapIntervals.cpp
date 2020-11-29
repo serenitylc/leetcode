a4c99e7ffc49aeb2c2aadd2b910e6e1d91c90653
LiYiShen <1757921819@qq.com>
2020-11-29 17:29:21 +0800

    435

--- Leetcode_with_Cpp/435eraseOverlapIntervals.cpp
+++ Leetcode_with_Cpp/435eraseOverlapIntervals.cpp
@@ -7,6 +7,7 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.size() == 0) return 0;
         sort(intervals.begin(), intervals.end(), cmp);
+        // sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; });
         int res = 1;
         int end = intervals[0][1];
         for (int i = 1; i < intervals.size(); i++)
