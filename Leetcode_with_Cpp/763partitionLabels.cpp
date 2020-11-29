0e6c43f9d7ecf05b1bd81625ab26f25d66bc3b25
LiYiShen <1757921819@qq.com>
2020-11-29 17:29:35 +0800

    763

---
+++ Leetcode_with_Cpp/763partitionLabels.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        vector<int> res;
+        unordered_map<char, int> mp;  // 记录字母对应的最后出现位置
+        for (int i = 0; i < S.size(); ++i) mp[S[i]] = i;
+        int start = 0, end = 0;
+        for (int i = 0; i < S.size(); ++i) {
+            end = max(end, mp[S[i]]);
+            if (i == end) {
+                res.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return res;
+    }
+};
