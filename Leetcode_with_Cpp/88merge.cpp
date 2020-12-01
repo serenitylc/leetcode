cb988a71d803d3b2a333f967689c0e8b011f4f5a
LiYiShen <1757921819@qq.com>
2020-12-01 16:50:26 +0800

    88

--- Leetcode_with_Cpp/88merge.cpp
+++ Leetcode_with_Cpp/88merge.cpp
@@ -2,11 +2,22 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int last = m + n - 1;
-        while(n) {
-            if(m == 0 || nums1[m - 1] <= nums2[n - 1])
+        while (n) {
+            if (m == 0 || nums1[m - 1] <= nums2[n - 1])
                 nums1[last--] = nums2[--n];
             else
                 nums1[last--] = nums1[--m];
         }
+    }
+};
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pos = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+        while (n >= 0) nums1[pos--] = nums2[n--];
     }
 };
