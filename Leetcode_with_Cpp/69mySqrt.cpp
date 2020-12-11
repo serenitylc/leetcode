dcbcc40e3ee0e3c25b6e39af534cb3b086c8c920
LiYiShen <1757921819@qq.com>
2020-12-11 15:54:04 +0800

    69

---
+++ Leetcode_with_Cpp/69mySqrt.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return x;
+        int l = 1, r = x, mid, sqrt;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            sqrt = x / mid;
+            if (sqrt == mid) return mid;
+            else if (mid > sqrt) r = mid - 1;
+            else l = mid + 1;
+        }
+        return r;
+    }
+};
+/* Å£¶Ùµü´ú·¨ */
+class Solution {
+public:
+    int mySqrt(int x) {
+        long a = x;
+        while (a * a > x) {
+            a = (a + x / a) / 2;
+        }
+        return a;
+    }
+};
