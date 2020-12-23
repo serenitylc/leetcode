1d94d24654e386086bcf2a3008973540f71c8424
LiYiShen <1757921819@qq.com>
2020-12-23 21:24:09 +0800

    Notes

--- Notes.cpp
+++ Notes.cpp
@@ -592,7 +592,7 @@ void bubble_sort(vector<int>& nums, int n) {
 	bool swapped;
 	for (int i = 1; i < n; ++i) {
 		swapped = false;
-		for (int j = 1; j < n - i - 1; ++j) {
+		for (int j = 1; j < n - i + 1; ++j) {
 			if (nums[j] < nums[j - 1]) {
 				swap(nums[j], nums[j - 1]);
 				swapped = true;
