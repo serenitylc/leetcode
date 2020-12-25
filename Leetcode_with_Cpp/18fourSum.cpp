33085bed8f4f1d8f218c33a88605523287335c9c
LiYiShen <1757921819@qq.com>
2020-12-25 23:03:37 +0800

    18

--- Leetcode_with_Cpp/18fourSum.cpp
+++ Leetcode_with_Cpp/18fourSum.cpp
@@ -4,23 +4,23 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
         int n = nums.size();
-        for(int i = 0; i < n - 3; i++) {
-            if(i > 0 && nums[i] == nums[i-1])
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1])
                 continue;
-            for(int j = i + 1; j < n - 2; j++) {
-                if(j > i + 1 && nums[j] == nums[j-1])
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1])
                     continue;
                 int left = j + 1, right = n - 1;
-                while(left < right) {
+                while (left < right) {
                     int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    if(sum < target)
-                        while(left < right && nums[left] == nums[++left]);
-                    else if(sum > target)
-                        while(left < right && nums[right] == nums[--right]);
-                    else{
+                    if (sum < target)
+                        while (left < right && nums[left] == nums[++left]);
+                    else if (sum > target)
+                        while (left < right && nums[right] == nums[--right]);
+                    else {
                         res.push_back(vector<int>{nums[i], nums[j], nums[left], nums[right]});
-                        while(left < right && nums[left] == nums[++left]);
-                        while(left < right && nums[right] == nums[--right]);
+                        while (left < right && nums[left] == nums[++left]);
+                        while (left < right && nums[right] == nums[--right]);
                     }
                 }
             }
