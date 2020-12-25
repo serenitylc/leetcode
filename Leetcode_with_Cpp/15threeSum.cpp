11c09dd2cb4673f4ee14091c96af61dedc2a01e3
LiYiShen <1757921819@qq.com>
2020-12-25 23:03:28 +0800

    15

--- Leetcode_with_Cpp/15threeSum.cpp
+++ Leetcode_with_Cpp/15threeSum.cpp
@@ -3,22 +3,22 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) 
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) 
                 continue;
             int l = i + 1, r = nums.size() - 1;
-            while(l < r) {
-                if(nums[l] + nums[r] + nums[i] < 0) 
+            while (l < r) {
+                if (nums[l] + nums[r] + nums[i] < 0) 
                     ++l;
-                else if(nums[l] + nums[r] + nums[i] > 0) 
+                else if (nums[l] + nums[r] + nums[i] > 0) 
                     --r;
-                else{
+                else {
                     res.push_back({nums[i], nums[l], nums[r]});
                     ++l, 
                     --r;
-                    while(l < r && nums[l] == nums[l - 1]) 
+                    while (l < r && nums[l] == nums[l - 1]) 
                         ++l;
-                    while(l < r && nums[r] == nums[r + 1]) 
+                    while (l < r && nums[r] == nums[r + 1]) 
                         --r;
                 }
             }
