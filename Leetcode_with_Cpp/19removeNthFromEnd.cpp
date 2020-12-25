7ee701164349fc5b17297d368c4dec7c5632bc74
LiYiShen <1757921819@qq.com>
2020-12-25 23:03:41 +0800

    19

--- Leetcode_with_Cpp/19removeNthFromEnd.cpp
+++ Leetcode_with_Cpp/19removeNthFromEnd.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head | !head -> next) 
-            return NULL;
+        if (!head | !head -> next) return NULL;
         ListNode *fast = head, *slow = head;
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             fast = fast -> next;
-        }
-        if(!fast) {
-            return head -> next;    
-        }
-        while(fast -> next) {
+        if (!fast) return head -> next;    
+        while (fast -> next) {
             fast = fast -> next;
             slow = slow -> next;
         }
