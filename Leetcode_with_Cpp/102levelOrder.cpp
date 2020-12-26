762c0b37beecef4a240390f95d0e2de1ea3a3fa0
LiYiShen <1757921819@qq.com>
2020-12-26 21:50:13 +0800

    102

--- Leetcode_with_Cpp/102levelOrder.cpp
+++ Leetcode_with_Cpp/102levelOrder.cpp
@@ -11,18 +11,18 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        if(!root)  return res;
+        if (!root)  return res;
         queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()) {
+        while (!q.empty()) {
             vector<int> temp;
             int len = q.size();
-            for(int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++) {
                 TreeNode* node = q.front();
                 q.pop();
                 temp.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
             res.push_back(temp);
         }
