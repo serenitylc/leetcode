eb0768388e5ae5fcc74389607bb348ad2e5c254f
LiYiShen <1757921819@qq.com>
2020-12-26 21:50:28 +0800

    112

--- Leetcode_with_Cpp/112hasPathSum.cpp
+++ Leetcode_with_Cpp/112hasPathSum.cpp
@@ -10,16 +10,11 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        if(root == NULL)
-            return false;
-
-        if(root->left == NULL && root->right == NULL)
-            return root->val == sum;
-        
+        if (root == NULL) return false;
+        if (root->left == NULL && root->right == NULL) return root->val == sum;
         //  return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
-        if(hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val))
+        if (hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val))
             return true;
-        
         return false;
     }
 };
