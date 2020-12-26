a6739045a290ae034aa67b9d466b48d1eb64ccd5
LiYiShen <1757921819@qq.com>
2020-12-26 21:50:37 +0800

    116

--- Leetcode_with_Cpp/116connect.cpp
+++ Leetcode_with_Cpp/116connect.cpp
@@ -19,14 +19,12 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if (root == NULL)
-            return root;
+        if (root == NULL) return root;
         connectTwoNode(root->left, root->right);
         return root;
     }
     void connectTwoNode(Node* node1, Node* node2) {
-        if (node1 == NULL || node2 == NULL)
-            return;
+        if (node1 == NULL || node2 == NULL) return;
         node1->next = node2;
         connectTwoNode(node1->left, node1->right);
         connectTwoNode(node2->left, node2->right);
@@ -37,8 +35,7 @@ public:
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        if (root == NULL || root->left == NULL)
-            return;
+        if (root == NULL || root->left == NULL) return;
         root->left->next = root->right;
         if (root->next)
             root->right->next = root->next->left;
@@ -50,8 +47,7 @@ public:
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        if (root == NULL)
-            return;
+        if (root == NULL) return;
         TreeLinkNode* pre = root;
         TreeLinkNode* cur = NULL;
         while (pre->left) {
