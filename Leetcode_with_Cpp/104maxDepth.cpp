65c7665f23bafb760911a5a38ccc21d4e7541e73
LiYiShen <1757921819@qq.com>
2020-12-26 21:50:18 +0800

    104

--- Leetcode_with_Cpp/104maxDepth.cpp
+++ Leetcode_with_Cpp/104maxDepth.cpp
@@ -10,28 +10,25 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL)
-            return 0;
+        if (root == NULL) return 0;
         return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
 
-
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL)
-            return 0;
+        if (root == NULL) return 0;
         int num = 0;
         queue<TreeNode *> que;
         que.push(root);
-        while(!que.empty()){
+        while (!que.empty()){
             int n = que.size();
-            for(int i = 0; i < n; ++i){
+            for (int i = 0; i < n; ++i) {
                 TreeNode *cur = que.front();
-                if(cur->left != NULL)
+                if (cur->left != NULL)
                     que.push(cur->left);
-                if(cur->right != NULL)
+                if (cur->right != NULL)
                     que.push(cur->right);
                 que.pop();
             }
