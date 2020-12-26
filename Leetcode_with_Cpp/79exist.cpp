5758ad6bf3bfe950afed78ca56a23a4bf5ab8544
LiYiShen <1757921819@qq.com>
2020-12-26 21:49:58 +0800

    79

--- Leetcode_with_Cpp/79exist.cpp
+++ Leetcode_with_Cpp/79exist.cpp
@@ -8,14 +8,14 @@ private:
     }
     // ´Ó board[x][y] ¿ªÊ¼£¬Ñ°ÕÒ word[index...word.size()]
     bool searchWord(const vector<vector<char>>& board, const string& word, int index, int x, int y) {
-        if(index == word.size() - 1)
+        if (index == word.size() - 1)
             return board[x][y] == word[index];
-        if(board[x][y] == word[index]) {
+        if (board[x][y] == word[index]) {
             visited[x][y] = true;
-            for(int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int newX = x + d[i][0];
                 int newY = y + d[i][1];
-                if(isArea(newX, newY) && !visited[newX][newY] && searchWord(board, word, index+1, newX, newY))
+                if (isArea(newX, newY) && !visited[newX][newY] && searchWord(board, word, index+1, newX, newY))
                     return true;
             }
             visited[x][y] = false;
@@ -28,9 +28,9 @@ public:
         assert(m > 0);
         n = board[0].size();
         visited = vector<vector<bool>>(m, vector<bool>(n, false));
-        for(int i = 0; i < board.size(); i++)
-            for(int j = 0; j < board[i].size(); j++)
-                if(searchWord(board, word, 0, i, j))
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board[i].size(); j++)
+                if (searchWord(board, word, 0, i, j))
                     return true;
         return false;
     }
