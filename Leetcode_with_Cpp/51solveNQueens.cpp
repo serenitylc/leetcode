f3a902380673e6dcbe6e7a33b0e93ffd439e685e
LiYiShen <1757921819@qq.com>
2020-12-29 18:55:48 +0800

    51

--- Leetcode_with_Cpp/51solveNQueens.cpp
+++ Leetcode_with_Cpp/51solveNQueens.cpp
@@ -14,7 +14,8 @@ public:
         }
         int n = board[row].size();
         for (int col = 0; col < n; col++) {
-            if (!isValid(board, row, col)) continue;
+            if (!isValid(board, row, col))
+                continue;
             board[row][col] = 'Q';
             backtrack(board, row + 1);
             board[row][col] = '.';
@@ -35,8 +36,6 @@ public:
     }
 };
 
-
-
 #include<iostream>
 #include<vector>
 #include<cassert>
@@ -48,12 +47,12 @@ private:
     vector<bool> col, dia1, dia2;
     // 尝试在一个n皇后问题中，摆放第index行的皇后位置，row存储摆放列
     void putQueen(int n, int index, vector<int>& row) {
-        if(index == n) {
+        if (index == n) {
             res.push_back(generateBoard(n, row));
             return;
         }
-        for(int i = 0; i < n; i++)
-            if(!col[i] && !dia1[index+i] && !dia2[index-i+n-1]) {
+        for (int i = 0; i < n; i++)
+            if (!col[i] && !dia1[index+i] && !dia2[index-i+n-1]) {
                 row.push_back(i);
                 col[i] = true;  dia1[index+i] = true;  dia2[index-i+n-1] = true;
                 putQueen(n, index+1, row);
@@ -65,7 +64,7 @@ private:
     vector<string> generateBoard(int n, vector<int>& row) {
         assert(row.size() == n);
         vector<string> board(n, string(n, '.'));
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             board[i][row[i]] = 'Q';
         return board;
     }
@@ -84,8 +83,8 @@ public:
 int main() {
     int n = 4;
     vector<vector<string>> res = Solution().solveNQueens(n);
-    for(int i = 0; i < res.size(); i++) {
-        for(int j = 0; j < n; j++)
+    for (int i = 0; i < res.size(); i++) {
+        for (int j = 0; j < n; j++)
             cout << res[i][j] << endl;
         cout << endl;
     }
