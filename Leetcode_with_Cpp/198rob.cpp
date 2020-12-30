f45708d89dc6ae26c66c7e6fa06ef6472d30e38d
LiYiShen <1757921819@qq.com>
2020-12-30 19:18:36 +0800

    198

--- Leetcode_with_Cpp/198rob.cpp
+++ Leetcode_with_Cpp/198rob.cpp
@@ -4,12 +4,12 @@ private:
     vector<int> memo;
     // 考虑抢劫 nums[index...nums.size()] 这个范围的所有房子
     int tryRob(vector<int>& nums, int index) {
-        if(index >= nums.size())
+        if (index >= nums.size())
             return 0;
-        if(memo[index] != -1)
+        if (memo[index] != -1)
             return memo[index];
         int res = 0;
-        for(int i = index; i < nums.size(); i++)
+        for (int i = index; i < nums.size(); i++)
             res = max(res, nums[i] + tryRob(nums, i + 2));
         memo[index] = res;
         return res;
@@ -24,12 +24,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0)
-            return 0;
+        if (n == 0) return 0;
         vector<int> memo(n, -1);
         memo[n-1] = nums[n-1];
-        for(int i = n - 2; i >= 0; i--)
-            for(int j = i; j < n; j++)
+        for (int i = n - 2; i >= 0; i--)
+            for (int j = i; j < n; j++)
                 memo[i] = max(memo[i], nums[j] + (j + 2 < n ? memo[j+2] : 0));
         return memo[0];
     }
