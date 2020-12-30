41a8b960472f92f99e9e97c1648b15ac8cdb811d
LiYiShen <1757921819@qq.com>
2020-12-30 19:18:24 +0800

    70

--- Leetcode_with_Cpp/70climbStairs.cpp
+++ Leetcode_with_Cpp/70climbStairs.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 2)
-            return n;
-        int num1 = 1;
-        int num2 = 2;
-        int numN = 0;
-        for(int i = 2; i < n; i++) {
-            numN = num1 + num2;
-            num1 = num2;
-            num2 = numN;
+        if (n <= 2) return n;
+        int pre2 = 1, pre1 = 2, cur;
+        for (int i = 2; i < n; ++i) {
+            cur = pre2 + pre1;
+            pre2 = pre1;
+            pre1 = cur;
         }
-        return numN;
+        return cur;
     }
 };
 
 class Solution {
 public:
     int climbStairs(int n) {
-
-        vector<int> memo(n+1, -1);
-        memo[0] = 1;
-        memo[1] = 2;
-        
-        for(int i = 2; i <= n; i++) 
-            memo[i] = memo[i-1] + memo[i-2];
-
-        return memo[n];
+        if (n <= 2) return n;
+        vector<int> dp(n+1, 1);
+        for (int i = 2; i <= n; ++i) 
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
