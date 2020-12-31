89c7b49ca2743272559c6c4abc48168b4a526cd3
LiYiShen <1757921819@qq.com>
2020-12-31 22:41:49 +0800

    219

--- Leetcode_with_Cpp/219containsNearbyDuplicate.cpp
+++ Leetcode_with_Cpp/219containsNearbyDuplicate.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> record;
-        for(int i = 0; i < nums.size(); i++) {
-            if(record.find(nums[i]) != record.end())
+        for (int i = 0; i < nums.size(); i++) {
+            if (record.find(nums[i]) != record.end())
                 return true;
-
             record.insert(nums[i]);
-
-            if(record.size() == k + 1)
+            if (record.size() == k + 1)
                 record.erase(nums[i - k]);
         }
         return false;
@@ -20,8 +18,8 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         map<int,int> record;
-        for(int i = 0; i < nums.size(); i++) {
-            if(record.count(nums[i]) && i - record[nums[i]] <= k)
+        for (int i = 0; i < nums.size(); i++) {
+            if (record.count(nums[i]) && i - record[nums[i]] <= k)
                 return true;
             else
                 record[nums[i]] = i;
