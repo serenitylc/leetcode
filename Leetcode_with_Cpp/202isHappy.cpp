87a3d9703be2d77e953c72d146c74a1bf43c9e3b
LiYiShen <1757921819@qq.com>
2020-12-31 22:41:31 +0800

    202

--- Leetcode_with_Cpp/202isHappy.cpp
+++ Leetcode_with_Cpp/202isHappy.cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
     int bitSquareSum(int n) {
         int sum = 0;
-        while(n > 0) {
+        while (n > 0) {
             int bit = n % 10;
             sum += bit * bit;
             n = n / 10;
@@ -29,7 +29,7 @@ public:
             slow = bitSquareSum(slow);
             fast = bitSquareSum(fast);
             fast = bitSquareSum(fast);
-        } while(slow != fast);
+        } while (slow != fast);
         return slow == 1;
     }
 };
