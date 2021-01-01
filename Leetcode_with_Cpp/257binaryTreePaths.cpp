14b5ea0904d115b40ed1a25a42d7ffec17a8d9d1
LiYiShen <1757921819@qq.com>
2021-01-01 17:31:49 +0800

    257

--- Leetcode_with_Cpp/257binaryTreePaths.cpp
+++ Leetcode_with_Cpp/257binaryTreePaths.cpp
@@ -11,22 +11,17 @@ class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> res;
-        if(root == NULL)
-            return res;
-        
-        if(root->left == NULL && root->right == NULL) {
+        if (root == NULL) return res;
+        if (root->left == NULL && root->right == NULL) {
             res.push_back(to_string(root->val));
             return res;
         }
-
         vector<string> leftS = binaryTreePaths(root->left);
-        for(int i = 0; i < leftS.size(); i++)
+        for (int i = 0; i < leftS.size(); i++)
             res.push_back(to_string(root->val) + "->" + leftS[i]);
-
         vector<string> rightS = binaryTreePaths(root->right);
-        for(int i = 0; i < rightS.size(); i++)
+        for (int i = 0; i < rightS.size(); i++)
             res.push_back(to_string(root->val) + "->" + rightS[i]);
-
         return res;
     }
 };
