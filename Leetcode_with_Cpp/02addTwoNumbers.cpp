c0222bbf103d277e043260ebaba4f47aaaa8277f
LiYiShen <1757921819@qq.com>
2021-01-01 17:31:12 +0800

    02

--- Leetcode_with_Cpp/02addTwoNumbers.cpp
+++ Leetcode_with_Cpp/02addTwoNumbers.cpp
@@ -16,16 +16,13 @@ public:
             if (l1 != nullptr) tmp += l1->val;
             if (l2 != nullptr) tmp += l2->val;
             tmp += flag;
-
             flag = tmp / 10;
             tmp %= 10; 
-
             // 利用已有节点构成新链条 破坏了原链表结构 节约了空间
             ListNode *next = l1 ? l1 : l2;
             if (next == nullptr) next = new ListNode(tmp); 
             // ListNode *next = new ListNode(tmp);
             next->val = tmp;
-            
             p->next = next;
             p = p->next;
             l1 = l1 ? l1->next : nullptr;
