a6f2a01956761283c55bbb115b91718ea9a7e839
LiYiShen <1757921819@qq.com>
2021-01-01 17:32:12 +0800

    343

--- Leetcode_with_Cpp/343integerBreak.cpp
+++ Leetcode_with_Cpp/343integerBreak.cpp
@@ -7,13 +7,10 @@ private:
     }
     // 将n进行分割（至少分割两部分），可以获得的最大乘积
     int breakInteger(int n) {
-        if(n == 1)
-            return 1;
-        if(memo[n] != -1)
-            return memo[n];
-
+        if (n == 1) return 1;
+        if (memo[n] != -1) return memo[n];
         int res = -1;
-        for(int i = 1; i <= n-1; i++)
+        for (int i = 1; i <= n-1; i++)
             res = max3(res, i * (n-i), i * breakInteger(n-i));
         memo[n] = res;
         return res;
@@ -38,9 +35,9 @@ public:
         // memo[i]表示将数字i分割（至少分割成两部分）后得到的最大乘积
         vector<int> memo(n+1, -1);
         memo[1] = 1;
-        for(int i = 2; i <= n; i++)
+        for (int i = 2; i <= n; i++)
             //求解memo[i]
-            for(int j = 1; j <= i-1; j++)
+            for (int j = 1; j <= i-1; j++)
                 memo[i] = max3(memo[i], j*(i-j), j*memo[i-j]);
         return memo[n];
     }
