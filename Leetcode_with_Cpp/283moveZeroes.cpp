b235cb9819390bab397bfb7926167a0d15454245
LiYiShen <1757921819@qq.com>
2021-01-01 17:31:58 +0800

    283

--- Leetcode_with_Cpp/283moveZeroes.cpp
+++ Leetcode_with_Cpp/283moveZeroes.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i,j=0;
-        for(i=0; i<nums.size(); i++) {
-            if(nums[i]!=0)
+        int i, j = 0;
+        for (i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0)
                 nums[j++] = nums[i];
         }
-        while(j<nums.size())
+        while (j<nums.size())
             nums[j++] = 0;
     }
 };
@@ -14,10 +14,10 @@ public:
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i,j=0;
-        for(i=0; i<nums.size(); i++) {
-            if(nums[i])
-                if(i!=j)
+        int i, j = 0;
+        for (i = 0; i < nums.size(); i++) {
+            if (nums[i])
+                if (i != j)
                     swap(nums[j++], nums[i]);
                 else
                     j++;
