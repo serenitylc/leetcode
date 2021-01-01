f2a68161598f01c82bffc8ca9bb5307b76385f49
LiYiShen <1757921819@qq.com>
2021-01-01 17:31:25 +0800

    220

--- Leetcode_with_Cpp/220containsNearbyAlmostDuplicate.cpp
+++ Leetcode_with_Cpp/220containsNearbyAlmostDuplicate.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         set<long long> record;
-        for(int i = 0; i < nums.size(); i++) {
-            if(record.lower_bound((long long)nums[i] - (long long)t) != record.end() 
+        for (int i = 0; i < nums.size(); i++) {
+            if (record.lower_bound((long long)nums[i] - (long long)t) != record.end() 
                     && *record.lower_bound((long long)nums[i] - (long long)t) <= (long long)nums[i] + (long long)t)  // 可能溢出
                 return true;
-
             record.insert(nums[i]);
-
-            if(record.size() == k + 1)
+            if (record.size() == k + 1)
                 record.erase(nums[i - k]);
         }
         return false;
@@ -19,15 +17,14 @@ public:
 // lower_bound(): 返回值是一个迭代器, 返回指向大于等于key的第一个值的位置 (要求有序数组)
 // upper_bound(): 返回大于等于key的最后一个元素的位置(要求有序数组)
 
-
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        if(k == 10000)
+        if (k == 10000)
             return false;
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = i + 1; j <= i + k && j < nums.size(); j++) {
-                if(abs((long)nums[i] - (long)nums[j]) <= t)
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j <= i + k && j < nums.size(); j++) {
+                i f(abs((long)nums[i] - (long)nums[j]) <= t)
                     return true;
             }
         }
