c6f92f3642abf366648829580bfdf75667e7d6ab
LiYiShen <1757921819@qq.com>
2021-01-01 17:32:08 +0800

    337

--- Leetcode_with_Cpp/337rob.cpp
+++ Leetcode_with_Cpp/337rob.cpp
@@ -11,9 +11,8 @@ class Solution {
 public:
     unordered_map<TreeNode*, int> memo;
     int rob(TreeNode* root) {
-        if(!root)
-            return 0;
-        if(memo[root])
+        if (!root) return 0;
+        if (memo[root])
             return memo[root];
         int do_it = root->val + (root->left == NULL ? 0 : rob(root->left->left) + rob(root->left->right)) + 
                                 (root->right == NULL ? 0 : rob(root->right->left) + rob(root->right->right));
