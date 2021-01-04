44316b1322ce38e3be59bd6d25993bad8d5b17bb
LiYiShen <1757921819@qq.com>
2021-01-04 17:01:52 +0800

    589

--- Leetcode_with_Cpp/589preorder.cpp
+++ Leetcode_with_Cpp/589preorder.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> res;
     vector<int> preorder(Node* root) {
-        if(!root)   return res;
+        if (!root) return res;
         res.push_back(root->val);
-        for(auto i : root->children){
+        for (auto i : root->children) {
             preorder(i);
         }
         return res;
@@ -15,14 +15,14 @@ class Solution {
 public:
     vector<int> preorder(Node* root) {
         vector<int> res;
-        if(!root)   return res;
+        if (!root) return res;
         stack<Node*> stk;
         stk.push(root);
-        while(!stk.empty()){
+        while (!stk.empty()) {
             Node* tmp = stk.top();
             stk.pop();
             res.push_back(tmp->val);
-            for(int i = tmp->children.size()-1; i >= 0; --i){
+            for (int i = tmp->children.size()-1; i >= 0; --i) {
                 stk.push(tmp->children[i]);
             }
         }
