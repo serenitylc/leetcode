da03191d76f963eb4200bcbd5cd17c7837827d93
LiYiShen <1757921819@qq.com>
2021-01-05 19:35:05 +0800

    24

--- Leetcode_with_Cpp/24swapPairs.cpp
+++ Leetcode_with_Cpp/24swapPairs.cpp
@@ -11,17 +11,14 @@ public:
     ListNode* swapPairs(ListNode* head) {
         ListNode* dHead = new ListNode(0);
         dHead->next = head;
-
         ListNode* p = dHead;
-        while(p->next && p->next->next) {
+        while (p->next && p->next->next) {
             ListNode* node1 = p->next;
             ListNode* node2 = node1->next;
             ListNode* next = node2->next;
-
             node2->next = node1;
             node1->next = next;
             p->next = node2;
-
             p = node1;
         }
         ListNode* resNode = dHead->next;
