e15c2eb57072fa22f410cad4f364e0ada7e932d9
LiYiShen <1757921819@qq.com>
2021-01-07 18:41:34 +0800

    40

--- Leetcode_with_Cpp/40combinationSum2.cpp
+++ Leetcode_with_Cpp/40combinationSum2.cpp
@@ -8,19 +8,18 @@ public:
         return res;
     }
     void backtrace(vector<int>& candidates, vector<int>& p, int pos, int target) {
-        if(target == 0) {
+        if (target == 0) {
             res.push_back(p);
             return;
         }
-        for(int i = pos; i < candidates.size(); i++) {
-            if(target - candidates[i] >= 0) {
+        for (int i = pos; i < candidates.size(); i++) {
+            if (target - candidates[i] >= 0) {
                 if (i != pos && candidates[i] == candidates[i-1]) // »•µÙ÷ÿ∏¥
                     continue;
                 p.push_back(candidates[i]);
                 backtrace(candidates, p, i + 1, target-candidates[i]);
                 p.pop_back();
-            }
-            else{ // ºÙ÷¶
+            } else { // ºÙ÷¶
                 break;
             }
         }
