982673762750c4cb9c80c4e3f793f8c581c80d09
LiYiShen <1757921819@qq.com>
2021-01-07 18:41:28 +0800

    39

--- Leetcode_with_Cpp/39combinationSum.cpp
+++ Leetcode_with_Cpp/39combinationSum.cpp
@@ -1,26 +1,21 @@
 class Solution {
 private:
     vector<vector<int>> res;
-    
     void backtrace(vector<int>& candidates, int target, vector<int>& p, int index) {
-        if(target < 0)
-            return;
-        if(target == 0) {
+        if (target < 0) return;
+        if (target == 0) {
             res.push_back(p);
             return;
         }
-        for(int i = index; i < candidates.size(); i++) {
+        for (int i = index; i < candidates.size(); i++) {
             p.push_back(candidates[i]);
             backtrace(candidates, target - candidates[i], p, i);
             p.pop_back();
         }
     }
-
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        if(candidates.size() == 0)
-            return res;
-        
+        if (candidates.size() == 0) return res;
         vector<int> p;
         backtrace(candidates, target, p, 0);
         return res;
