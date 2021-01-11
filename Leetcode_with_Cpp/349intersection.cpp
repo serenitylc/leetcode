8ca19f742d19d5c17df0016543bdf7b99309e05f
LiYiShen <1757921819@qq.com>
2021-01-11 16:53:27 +0800

    349

--- Leetcode_with_Cpp/349intersection.cpp
+++ Leetcode_with_Cpp/349intersection.cpp
@@ -13,7 +13,6 @@ public:
             record.insert(nums1[i]);
         */
         set<int> record(nums1.begin(), nums1.end());
-
         set<int> resultSet;
         for (int i = 0; i < nums2.size(); i++)
             if (record.find(nums2[i]) != record.end())
@@ -24,7 +23,6 @@ public:
             resultVector.push_back(*iter);
         return resultVector;
         */
-        
         return vector<int>(resultSet.begin(), resultSet.end());
     }
 };
