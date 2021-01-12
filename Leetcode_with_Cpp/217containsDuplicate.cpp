74227d818ead87b5d6ebc7a0bec223120efd3d84
LiYiShen <1757921819@qq.com>
2021-01-12 16:56:57 +0800

    217

---
+++ Leetcode_with_Cpp/217containsDuplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        return s.size() != nums.size();
+    }
+};
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int & num : nums) {
+            if (s.count(num))
+                return true;
+            s.insert(num);
+        }
+        return false;
+    }
+};
