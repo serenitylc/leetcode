aed9ad023f2270bb81443af48b975567cf70fc0f
LiYiShen <1757921819@qq.com>
2021-01-14 19:31:55 +0800

    448

--- Leetcode_with_Cpp/448findDisappearedNumbers.cpp
+++ Leetcode_with_Cpp/448findDisappearedNumbers.cpp
@@ -8,7 +8,8 @@ public:
             if (nums[index] > 0) nums[index] *= -1;
         }
         for (int i = 0; i < n; i++)
-            if (nums[i] > 0) res.push_back(i + 1);
+            if (nums[i] > 0)
+                res.push_back(i + 1);
         return res;
     }
 };
