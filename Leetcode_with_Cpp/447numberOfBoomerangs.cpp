688c3b092b901dbfd7bbdad820b47798856d66ef
LiYiShen <1757921819@qq.com>
2021-01-14 19:31:50 +0800

    447

--- Leetcode_with_Cpp/447numberOfBoomerangs.cpp
+++ Leetcode_with_Cpp/447numberOfBoomerangs.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int numberOfBoomerangs(vector<vector<int>>& points) {
         int res = 0;
-        for(int i = 0; i < points.size(); i++) {        //选择第一个点
+        for (int i = 0; i < points.size(); i++) {        //选择第一个点
             unordered_map<int, int> um;                 //距离, 个数
-            for(int j = 0; j < points.size(); j++) {    //选择第二个点
-                if(i == j) continue;                    //如果i和j指向同一点, 跳过
+            for (int j = 0; j < points.size(); j++) {    //选择第二个点
+                if (i == j) continue;                    //如果i和j指向同一点, 跳过
                 int dist2 = (points[i][0] - points[j][0]) * (points[i][0] - points[j][0]) + 
                             (points[i][1] - points[j][1]) * (points[i][1] - points[j][1]);  //计算距离
                 um[dist2] ++;
             }
-            for(auto p : um) {
+            for (auto p : um) {
                 res += p.second * (p.second - 1);
             }
         }
