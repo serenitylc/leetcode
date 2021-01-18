393737a464865ae9773a52e26db6a2e023225d60
LiYiShen <1757921819@qq.com>
2021-01-18 16:29:12 +0800

    763

--- Leetcode_with_Cpp/763partitionLabels.cpp
+++ Leetcode_with_Cpp/763partitionLabels.cpp
@@ -3,7 +3,8 @@ public:
     vector<int> partitionLabels(string S) {
         vector<int> res;
         unordered_map<char, int> mp;  // 记录字母对应的最后出现位置
-        for (int i = 0; i < S.size(); ++i) mp[S[i]] = i;
+        for (int i = 0; i < S.size(); ++i)
+            mp[S[i]] = i;
         int start = 0, end = 0;
         for (int i = 0; i < S.size(); ++i) {
             end = max(end, mp[S[i]]);
