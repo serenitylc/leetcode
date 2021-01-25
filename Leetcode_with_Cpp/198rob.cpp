87158307c72340b55a822b5100dfa4ffd9e00840
LiYiShen <1757921819@qq.com>
2021-01-25 20:04:12 +0800

    198

--- Leetcode_with_Cpp/198rob.cpp
+++ Leetcode_with_Cpp/198rob.cpp
@@ -1,35 +1,12 @@
 class Solution {
-private:
-    // memo[i] 表示考虑抢劫 nums[i..n) 所能获得的最大收益
-    vector<int> memo;
-    // 考虑抢劫 nums[index...nums.size()] 这个范围的所有房子
-    int tryRob(vector<int>& nums, int index) {
-        if (index >= nums.size())
-            return 0;
-        if (memo[index] != -1)
-            return memo[index];
-        int res = 0;
-        for (int i = index; i < nums.size(); i++)
-            res = max(res, nums[i] + tryRob(nums, i + 2));
-        memo[index] = res;
-        return res;
-    }
 public:
     int rob(vector<int>& nums) {
-        memo = vector<int>(nums.size(), -1);
-        return tryRob(nums, 0);
-    }
-};
-class Solution {
-public:
-    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
         int n = nums.size();
-        if (n == 0) return 0;
-        vector<int> memo(n, -1);
-        memo[n-1] = nums[n-1];
-        for (int i = n - 2; i >= 0; i--)
-            for (int j = i; j < n; j++)
-                memo[i] = max(memo[i], nums[j] + (j + 2 < n ? memo[j+2] : 0));
-        return memo[0];
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for (int i = 2; i <= n; ++i)
+            dp[i] = max(dp[i-1], nums[i-1] + dp[i-2]);
+        return dp[n];          
     }
 };
