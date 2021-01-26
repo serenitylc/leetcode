faf381150d332a15e27311cce3cec261e6f9c21b
LiYiShen <1757921819@qq.com>
2021-01-26 20:07:13 +0800

    204

---
+++ Leetcode_with_Cpp/204countPrimes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> isPrime(n, true);
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i]) {
+                ++count;
+                for (int j = 2; i * j < n; ++j)
+                    isPrime[i*j] = false;
+            }
+        }
+        return count;
+    }
+};
