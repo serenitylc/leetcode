b00e8924d1f3e4a64f12924174eab47b3c06276f
LiYiShen <1757921819@qq.com>
2021-01-29 18:13:21 +0800

    21

--- Leetcode_with_Cpp/21mergeTwoLists.cpp
+++ Leetcode_with_Cpp/21mergeTwoLists.cpp
@@ -9,23 +9,37 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* p = l1;
-        ListNode* q = l2;
-        ListNode* result = new ListNode(0);
-        ListNode* r = result;
+        ListNode *p = l1, *q = l2;
+        ListNode* res = new ListNode(0);
+        ListNode* cur = res;
         while (p && q) {
             if (q->val > p->val) {
-                r->next = p;
-                r = r->next;
+                cur->next = p;
+                cur = cur->next;
                 p = p->next;
             } else {
-                r->next = q;
-                r = r->next;
+                cur->next = q;
+                cur = cur->next;
                 q = q->next;
             }
         }
-        if (q) r->next = q;
-        if (p) r->next = p;
-        return result->next;
+        if (q) cur->next = q;
+        if (p) cur->next = p;
+        return res->next;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* p1, ListNode* p2) {
+        if (!p1) return p2;
+        if (!p2) return p1;
+        if (p1->val <= p2->val) {
+            p1->next = mergeTwoLists(p1->next, p2);
+            return p1;
+        } else {
+            p2->next = mergeTwoLists(p1, p2->next);
+            return p2;
+        }
     }
 };
