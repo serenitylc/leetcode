bad8690205fb9a4e2c4b8e0c11bccc2ae2c66ac2
LiYiShen <1757921819@qq.com>
2021-02-20 16:44:15 +0800

    34

--- Leetcode_with_Cpp/34searchRange.cpp
+++ Leetcode_with_Cpp/34searchRange.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         if (nums.empty()) return {-1, -1};
 		int l = 0, r = nums.size() - 1;
-		while (l < r) {  // 找到起始位置 
+		while (l < r) {  // 找到起始位置
 			int mid = l + r >> 1;
 			if (nums[mid] >= target) r = mid;
 			else l = mid + 1;
@@ -12,7 +12,7 @@ public:
 		int start = l;
 		r = nums.size() - 1;
 		while (l < r) {  // 找到终点位置
-			int mid = l +  r + 1 >> 1;  // 不加括号效率更高
+			int mid = l + r + 1 >> 1;  // 不加括号效率更高
 			if (nums[mid] > target) r = mid - 1;
 			else l = mid;
 		}
