32d53b486265b55d5b4410ed336b2d5b0370e3e5
LiYiShen <1757921819@qq.com>
2021-02-20 16:44:21 +0800

    75

--- Leetcode_with_Cpp/75sortColors.cpp
+++ Leetcode_with_Cpp/75sortColors.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int j=0;
+        // 计数排序
         int count[3] = {0};
-        for(int i=0; i<nums.size(); i++) // 计数排序
+        for (int i = 0; i < nums.size(); ++i)
             count[nums[i]]++;
-        while(j < nums.size()) {
-            if(j < count[0])
+        int j = 0;
+        while (j < nums.size()) {
+            if (j < count[0])
                 nums[j++] = 0;
-            else if(j < count[1] + count[0])
+            else if (j < count[1] + count[0])
                 nums[j++] = 1;
             else
                 nums[j++] = 2;
@@ -16,17 +17,16 @@ public:
     }
 };
 
-
-// 三路快排
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // 三路快排
         int zero = -1;           // nums[0...zero] == 0
         int two = nums.size();   // nums[two...n-1] == 2
-        for(int i=0; i<two; ) {  // nums[i] == 2 时 i 不增加
-            if(nums[i] == 1)
+        for (int i = 0; i < two; ) {
+            if (nums[i] == 1)
                 i++;
-            else if(nums[i] == 2)
+            else if (nums[i] == 2)  // nums[i] == 2 时 i 不增加
                 swap(nums[i], nums[--two]);
             else
                 swap(nums[i++], nums[++zero]);
