f483d4aa4023b87d6bf291100152b2a1f019b6ae
LiYiShen <1757921819@qq.com>
2021-02-20 16:44:27 +0800

    153

---
+++ Leetcode_with_Cpp/153findMin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) return nums[left];
+            int mid = left + right >> 1;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+};
