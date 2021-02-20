05f3792cb8689e8e5a5b695600ab8eacfbb4a6ab
LiYiShen <1757921819@qq.com>
2021-02-20 16:44:42 +0800

    215

--- Leetcode_with_Cpp/215findKthLargest.cpp
+++ Leetcode_with_Cpp/215findKthLargest.cpp
@@ -24,3 +24,10 @@ public:
         return j;
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return sort(nums.begin(), nums.end(), greater<int>()), nums[k - 1];
+    }
+};
