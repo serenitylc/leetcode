ac94e250b37f231f955f7fc2e83a0222f085f44f
LiYiShen <1757921819@qq.com>
2021-02-21 18:18:59 +0800

    680

---
+++ Leetcode_with_Cpp/680validPalindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isValid(s, left + 1, right) || isValid(s, left, right - 1);
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+    bool isValid(string & s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
