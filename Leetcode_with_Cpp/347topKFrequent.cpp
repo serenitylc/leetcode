1dfbde63561e3df8d073ea7ce0bb0cd7b031fb59
LiYiShen <1757921819@qq.com>
2021-02-21 18:18:36 +0800

    347

--- Leetcode_with_Cpp/347topKFrequent.cpp
+++ Leetcode_with_Cpp/347topKFrequent.cpp
@@ -5,8 +5,7 @@ public:
         // priority_queue< type, container, function >
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;  // 小顶堆(小值优先级高)
         vector<int> res;
-        for (int i = 0; i < nums.size(); i++)
-            mp[nums[i]]++;
+        for (int i = 0; i < nums.size(); i++) mp[nums[i]]++;
         for (unordered_map<int, int>::iterator it = mp.begin(); it != mp.end(); it++) {
             if (pq.size() == k) {
                 if (it->second > pq.top().first) {
