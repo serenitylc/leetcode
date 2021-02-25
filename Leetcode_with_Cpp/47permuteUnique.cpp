1cd4042bc1114229b0b29894493f88429e983be2
LiYiShen <1757921819@qq.com>
2021-02-25 20:07:52 +0800

    47

--- Leetcode_with_Cpp/47permuteUnique.cpp
+++ Leetcode_with_Cpp/47permuteUnique.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> re;
-        vector<int> used(nums.size(), 0);
+        vector<int> track;
+        vector<bool> visited(nums.size(), false);
         sort(nums.begin(), nums.end());
-        dfs(res, re, used, nums);
+        dfs(res, track, visited, nums);
         return res;
     }
-    void dfs(vector<vector<int>>& res, vector<int>& re, vector<int>& used, vector<int>& nums)
-    {
-        if (re.size() == nums.size()) {
-            res.push_back(re);
+    void dfs(vector<vector<int>>& res, vector<int>& track, vector<bool>& visited, vector<int>& nums) {
+        if (track.size() == nums.size()) {
+            res.push_back(track);
             return;
         }
-        int r = INT_MAX;
-        for (int i = 0; i < nums.size(); i++) {
-            if (used[i] == 0) {
-                // 针对重复数字漂亮的剪枝，前提是需要排序
-                if (nums[i] != r) {
-                    r = nums[i];
-                    re.push_back(nums[i]);
-                    used[i] = 1;
-                    dfs(res, re, used, nums);
-                    re.pop_back();
-                    used[i] = 0;
-                }
+        int last = INT_MAX;
+        for (int i = 0; i < nums.size(); ++i) {
+            // 保证下一次回溯的根与上一次不同
+            if (!visited[i] && nums[i] != last) {
+                last = nums[i];
+                track.push_back(nums[i]);
+                visited[i] = true;
+                dfs(res, track, visited, nums);
+                track.pop_back();
+                visited[i] = false;
             }
         }
     }
