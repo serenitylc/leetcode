d80c03a566070a47a7dc264cb39068b36ad67e2f
LiYiShen <1757921819@qq.com>
2021-02-25 20:08:03 +0800

    257

--- Leetcode_with_Cpp/257binaryTreePaths.cpp
+++ Leetcode_with_Cpp/257binaryTreePaths.cpp
@@ -17,10 +17,10 @@ public:
             return res;
         }
         vector<string> leftS = binaryTreePaths(root->left);
-        for (int i = 0; i < leftS.size(); i++)
+        for (int i = 0; i < leftS.size(); ++i)
             res.push_back(to_string(root->val) + "->" + leftS[i]);
         vector<string> rightS = binaryTreePaths(root->right);
-        for (int i = 0; i < rightS.size(); i++)
+        for (int i = 0; i < rightS.size(); ++i)
             res.push_back(to_string(root->val) + "->" + rightS[i]);
         return res;
     }
