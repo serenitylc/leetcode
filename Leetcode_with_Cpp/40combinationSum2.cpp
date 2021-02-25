7d186f42f010f63b93a1ebb81b1b488da717877f
LiYiShen <1757921819@qq.com>
2021-02-25 20:07:47 +0800

    40

--- Leetcode_with_Cpp/40combinationSum2.cpp
+++ Leetcode_with_Cpp/40combinationSum2.cpp
@@ -3,22 +3,22 @@ public:
     vector<vector<int>> res;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        vector<int> p;
-        backtrace(candidates, p, 0, target);
+        vector<int> track;
+        backtrace(candidates, track, 0, target);
         return res;
     }
-    void backtrace(vector<int>& candidates, vector<int>& p, int pos, int target) {
+    void backtrace(vector<int>& candidates, vector<int>& track, int pos, int target) {
         if (target == 0) {
-            res.push_back(p);
+            res.push_back(track);
             return;
         }
-        for (int i = pos; i < candidates.size(); i++) {
+        for (int i = pos; i < candidates.size(); ++i) {
             if (target - candidates[i] >= 0) {
                 if (i != pos && candidates[i] == candidates[i-1]) // »•µÙ÷ÿ∏¥
                     continue;
-                p.push_back(candidates[i]);
-                backtrace(candidates, p, i + 1, target-candidates[i]);
-                p.pop_back();
+                track.push_back(candidates[i]);
+                backtrace(candidates, track, i + 1, target-candidates[i]);
+                track.pop_back();
             } else { // ºÙ÷¶
                 break;
             }
