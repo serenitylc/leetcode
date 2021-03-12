8bdc23d9ce279af76918f270bd3dc296f38cf667
LiYiShen <1757921819@qq.com>
2021-03-12 13:03:38 +0800

    206

--- Leetcode_with_Cpp/206reverseList.cpp
+++ Leetcode_with_Cpp/206reverseList.cpp
@@ -17,7 +17,18 @@ public:
             curr->next = prev;
             prev = curr;
             curr = next;
-        }    
+        }
         return prev;
+    }
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
     }
 };
