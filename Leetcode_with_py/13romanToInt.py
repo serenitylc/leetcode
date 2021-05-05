37aa377da8d4b17ed3c84d3567e1ea227183187f
LiYiShen <1757921819@qq.com>
2021-05-05 15:01:41 +0800

    13

--- Leetcode_with_py/13romanToInt.py
+++ Leetcode_with_py/13romanToInt.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        map = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         sum = 0
         for i in range(len(s)):
-            if i<len(s)-1 and map[s[i]] < map[s[i+1]]:
+            if i < len(s) - 1 and map[s[i]] < map[s[i+1]]:
                 sum -= map[s[i]]
             else:
                 sum += map[s[i]]
