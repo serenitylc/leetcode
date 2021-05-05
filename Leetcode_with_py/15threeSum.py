4bf60448562bb0506f573419cda47fc195539913
LiYiShen <1757921819@qq.com>
2021-05-05 15:01:49 +0800

    15

--- Leetcode_with_py/15threeSum.py
+++ Leetcode_with_py/15threeSum.py
@@ -8,18 +8,18 @@ class Solution(object):
         res = []
         i = 0
         for i in range(len(nums)):
-            if i==0 or nums[i]>nums[i-1]:
+            if i == 0 or nums[i] > nums[i-1]:
                 l = i + 1
                 r = len(nums) - 1
                 while l < r:
                     s = nums[i] + nums[l] + nums[r]
-                    if s==0:
+                    if s == 0:
                         res.append([nums[i], nums[l], nums[r]])
                         l += 1
                         r -= 1
-                        while l < r and nums[l]==nums[l-1]:
+                        while l < r and nums[l] == nums[l-1]:
                             l += 1
-                        while l < r and nums[r]==nums[r+1]:
+                        while l < r and nums[r] == nums[r+1]:
                             r -= 1
                     elif s > 0:
                         r -= 1
@@ -27,6 +27,6 @@ class Solution(object):
                         l += 1
         return res
 
-# 1.将数组排序 
+# 1.将数组排序
 # 2.定义三个指针，i，j，k。
 # 遍历i，问题转化为在i之后的数组中寻找nums[j]+nums[k]=-nums[i]，即将三数之和问题转变为二数之和（使用双指针）
