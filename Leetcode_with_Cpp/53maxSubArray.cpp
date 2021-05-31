fdc02081e62fd03b2289d4ee877a8d28254b8129
LiYiShen <1757921819@qq.com>
2021-05-31 16:28:50 +0800

    53

--- Leetcode_with_Cpp/53maxSubArray.cpp
+++ Leetcode_with_Cpp/53maxSubArray.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if (nums.size() == 0)
-            return 0;
-        int res= nums[0];
+        if (nums.size() == 0) return 0;
+        int res = nums[0];
         vector<int> dp(nums.size(), res);
         for (int i = 1; i < nums.size(); i++) {
             dp[i] = max(dp[i-1] + nums[i], nums[i]);
