8665c34030a8260cfe9a2ce11672a72fd3ccbaa4
LiYiShen <1757921819@qq.com>
2021-06-02 14:37:03 +0800

    49

--- Leetcode_with_Cpp/49groupAnagrams.cpp
+++ Leetcode_with_Cpp/49groupAnagrams.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
         map<string, vector<string>> m;
-        for(int i = 0; i < strs.size(); i++) {
+        for (int i = 0; i < strs.size(); i++) {
             string key = strs[i];
             sort(key.begin(), key.end());
             m[key].push_back(strs[i]);
         }
-        for(auto it = m.begin(); it != m.end(); it ++)
+        for (auto it = m.begin(); it != m.end(); it ++)
             res.push_back(it->second);
         return res;
     }
