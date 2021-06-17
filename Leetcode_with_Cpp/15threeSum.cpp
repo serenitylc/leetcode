44b5b5044cb04af415612603fddead35a54f6a8e
LiYiShen <1757921819@qq.com>
2021-06-17 11:00:08 +0800

    15

--- Leetcode_with_Cpp/15threeSum.cpp
+++ Leetcode_with_Cpp/15threeSum.cpp
@@ -4,25 +4,25 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) 
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             int l = i + 1, r = nums.size() - 1;
             while (l < r) {
-                if (nums[l] + nums[r] + nums[i] < 0) 
+                if (nums[l] + nums[r] + nums[i] < 0)
                     ++l;
-                else if (nums[l] + nums[r] + nums[i] > 0) 
+                else if (nums[l] + nums[r] + nums[i] > 0)
                     --r;
                 else {
                     res.push_back({nums[i], nums[l], nums[r]});
-                    ++l, 
+                    ++l,
                     --r;
-                    while (l < r && nums[l] == nums[l - 1]) 
+                    while (l < r && nums[l] == nums[l - 1])
                         ++l;
-                    while (l < r && nums[r] == nums[r + 1]) 
+                    while (l < r && nums[r] == nums[r + 1])
                         --r;
                 }
             }
         }
-        return res; 
+        return res;
     }
 };
