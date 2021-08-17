cafe91fc69ce4fed1c7e9711f4b26714f066b938
serenityli <serenityli@tencent.com>
2021-08-17 20:11:23 +0800

    236

--- Leetcode_with_Cpp/236lowestCommonAncestor.cpp
+++ Leetcode_with_Cpp/236lowestCommonAncestor.cpp
@@ -10,7 +10,7 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) return NULL;
+        if (!root) return NULL;
         if (root->val == p->val || root->val == q->val)
             return root;
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
