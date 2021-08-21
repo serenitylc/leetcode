6d5922b6ba54706332ab25658dc5e1946f1543e9
LiYiShen <1757921819@qq.com>
2021-08-21 18:35:30 +0800

    124

--- Leetcode_with_Cpp/124maxPathSum.cpp
+++ Leetcode_with_Cpp/124maxPathSum.cpp
@@ -10,18 +10,17 @@
  * };
  */
 class Solution {
-private:
-    int res = INT_MIN;
 public:
     int maxPathSum(TreeNode* root) {
-        oneSideMax(root);
+        int res = INT_MIN;
+        oneSideMax(root, res);
         return res;
     }
-    int oneSideMax(TreeNode* root) {
-        if (root == nullptr) return 0;
-        int left = max(0, oneSideMax(root->left));  // 如果左右孩子的结果是负数, 则舍弃
-        int right = max(0, oneSideMax(root->right));
-        res = max(res, left + right + root->val);
-        return max(left, right) + root->val;
+    int oneSideMax(TreeNode* root, int & res) {
+        if (!root) return 0;
+        int l = max(0, oneSideMax(root->left, res));
+        int r = max(0, oneSideMax(root->right, res));
+        res = max(res, l + r + root->val);
+        return max(l, r) + root->val;
     }
 };
