f29fd8f61cf764c4dc50a357b1741479198618d1
serenityli <serenityli@tencent.com>
2021-08-23 17:41:18 +0800

    135

--- Leetcode_with_Cpp/135candy.cpp
+++ Leetcode_with_Cpp/135candy.cpp
@@ -4,7 +4,7 @@ public:
         int n = ratings.size();
         if (n < 2) return n;
         vector<int> nums(n, 1);
-        for (int i = 1; i < n; ++i)  // 贪心策略 每次遍历只考虑并更新相邻一侧的大小关系
+        for (int i = 1; i < n; ++i)  // 璐績绛栫暐 姣忔閬嶅巻鍙�冭檻骞舵洿鏂扮浉閭讳竴渚х殑澶у皬鍏崇郴
             if (ratings[i] > ratings[i - 1]) nums[i] = nums[i - 1] + 1;
         for (int i = n - 1; i > 0; --i)
             if (ratings[i - 1] > ratings[i]) nums[i - 1] = max(nums[i - 1], nums[i] + 1);
