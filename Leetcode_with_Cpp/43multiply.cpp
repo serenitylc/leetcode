bbc7756e23cf97e1c7cf4880c1040bdc35a1dd82
serenityli <serenityli@tencent.com>
2021-08-23 17:41:13 +0800

    43

---
+++ Leetcode_with_Cpp/43multiply.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int l1 = num1.size();
+        int l2 = num2.size();
+        string res(l1 + l2, '0');
+        for (int i = l1 - 1; i >= 0; i--) {
+            for (int j = l2 - 1; j >= 0; j--) {
+                int tmp = (res[i+j+1] - '0') + (num1[i] - '0') * (num2[j] - '0'); 
+                res[i+j+1] = tmp % 10 + '0';
+                res[i+j] += tmp / 10;
+            }
+        }
+        for (int i = 0; i < l1 + l2; i++) {
+            if (res[i] != '0') {
+                return res.substr(i);
+            }
+        }
+        return "0";  
+    }
+};
