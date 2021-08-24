42022a744db177e4e541cd02f72ab5c03fc4c95f
serenityli <serenityli@tencent.com>
2021-08-24 20:05:00 +0800

    08

--- Leetcode_with_Cpp/08myAtoi.cpp
+++ Leetcode_with_Cpp/08myAtoi.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     int myAtoi(string s) {
         long res = 0;
-        int state = 0;  //  0-³õÊ¼×´Ì¬  1-ÕýÊý  2-¸ºÊý
-        for (char c : s) {
-            if (state == 0 && c == ' ') continue;
-            else if (state == 0 && c == '+') state = 1;
-            else if (state == 0 && c == '-') state = 2;
-            else if (c >= '0' && c <= '9') {
+        int state = 0;  //  0-åˆå§‹çŠ¶æ€  1-æ­£æ•°  2-è´Ÿæ•°
+        for (char & c : s) {
+            if (state == 0 && c == ' ') {
+                continue;
+            } else if (state == 0 && c == '+') {
+                state = 1;
+            } else if (state == 0 && c == '-') {
+                state = 2;
+            } else if (c >= '0' && c <= '9') {
                 if (state == 0) state = 1;
                 int tmp = c - '0';
                 res = res * 10 + tmp;
                 if (res > INT_MAX) break;
-            } else break;
+            } else {
+                break;
+            }
         }
-        if (state == 1 && res > INT_MAX) res = INT_MAX;
-        else if (state == 2) {
+        if (state == 1 && res > INT_MAX) {
+            res = INT_MAX;
+        } else if (state == 2) {
             res = -res;
             if (res < INT_MIN) res = INT_MIN;
         }
