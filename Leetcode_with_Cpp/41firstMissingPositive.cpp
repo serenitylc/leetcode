024af3bbd708215abc6cadfd8f19f92b85387a41
serenityli <serenityli@tencent.com>
2021-08-24 20:05:19 +0800

    41

--- Leetcode_with_Cpp/41firstMissingPositive.cpp
+++ Leetcode_with_Cpp/41firstMissingPositive.cpp
@@ -8,7 +8,8 @@ public:
             }
         }
         for (int i = 0; i < n; ++i) {
-            if (nums[i] != i + 1) return i + 1;
+            if (nums[i] != i + 1)
+                return i + 1;
         }
         return n + 1;
     }
