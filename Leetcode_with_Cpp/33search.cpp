790826d8e78c53b9a6538c4130f74ce0b553fb81
serenityli <serenityli@tencent.com>
2021-08-24 20:05:14 +0800

    33

--- Leetcode_with_Cpp/33search.cpp
+++ Leetcode_with_Cpp/33search.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + right >> 1;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
             if (nums[mid] == target) return mid;
-            if (nums[mid] >= nums[left]) {  // mid 可能等于 left
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
+            if (nums[mid] >= nums[l]) {  // mid 鍙兘绛変簬 l
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1;
                 } else {
-                    left = mid + 1;
+                    l = mid + 1;
                 }
             } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
                 } else {
-                    right = mid - 1;
+                    r = mid - 1;
                 }
             }
         }
