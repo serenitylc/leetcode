74f132d0372d11c324c27d9b5fd8c86cc8f4fd68
LiYiShen <1757921819@qq.com>
2021-08-25 00:25:28 +0800

    43

--- Leetcode_with_Cpp/43multiply.cpp
+++ Leetcode_with_Cpp/43multiply.cpp
@@ -5,9 +5,9 @@ public:
         string res(l1 + l2, '0');
         for (int i = l1 - 1; i >= 0; i--) {
             for (int j = l2 - 1; j >= 0; j--) {
-                int tmp = (res[i+j+1] - '0') + (num1[i] - '0') * (num2[j] - '0'); 
-                res[i+j+1] = tmp % 10 + '0';
-                res[i+j] += tmp / 10;
+                int tmp = (res[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0');
+                res[i + j + 1] = tmp % 10 + '0';
+                res[i + j] += tmp / 10;
             }
         }
         for (int i = 0; i < l1 + l2; i++) {
@@ -15,6 +15,6 @@ public:
                 return res.substr(i);
             }
         }
-        return "0";  
+        return "0";
     }
 };
