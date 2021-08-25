33486dd6ea4ecbf149909e977c70b3b4108f7969
serenityli <serenityli@tencent.com>
2021-08-25 15:59:22 +0800

    98

--- Leetcode_with_Cpp/98isValidBST.cpp
+++ Leetcode_with_Cpp/98isValidBST.cpp
@@ -13,9 +13,9 @@ public:
         return isValidBST(root, NULL, NULL);
     }
     bool isValidBST(TreeNode* root, TreeNode* min, TreeNode* max) {
-        if (root == NULL) return true;
-        if (min != NULL && min->val >= root->val) return false;
-        if (max != NULL && max->val <= root->val) return false;
+        if (!root) return true;
+        if (min && min->val >= root->val) return false;
+        if (max && max->val <= root->val) return false;
         return isValidBST(root->left, min, root) && isValidBST(root->right, root, max);
     }
 };
