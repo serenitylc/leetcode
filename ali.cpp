09d4153eaaa0009f9337d6e7b3dc2b0cb4125aa9
serenityli <serenityli@tencent.com>
2021-09-01 17:31:03 +0800

    ali

--- ali.cpp
+++ ali.cpp
@@ -1,8 +1,8 @@
-�˹� SAY NO��
-���ݿ���ơ�������ô������ϵͳ����ģ����ƣ�����ҵ�����̣�
-timestamp/datatime ��ôѡ��
-go����ļ̳й�ϵ���ʵ��
-http/rpcЭ��
-ָ������õı�������
-I/O���õı��ʣ�select/poll/epoll�ı�������
-�����ڴ桢ҳ������㷨����ȱ��
+八股 SAY NO！
+数据库设计、索引怎么建立、系统功能模块设计（根据业务流程）
+timestamp/datatime 怎么选择
+go里面的继承关系如何实现
+http/rpc协议
+指针和引用的本质区别
+I/O复用的本质，select/poll/epoll的本质区别
+虚拟内存、页面调度算法及优缺点
