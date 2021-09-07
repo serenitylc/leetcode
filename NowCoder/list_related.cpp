34f0d76c405b36f8514111eaf42a2da302e49caa
LiYiShen <1757921819@qq.com>
2021-09-07 12:36:57 +0800

    list related

--- NowCoder/list_related.cpp
+++ NowCoder/list_related.cpp
@@ -47,6 +47,7 @@ public:
     }
 };
 
+// 区间反转
 class Solution {
 public:
     ListNode* lastNode;
@@ -278,5 +279,46 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.size() == 0) return nullptr;
         return merge(lists, 0, lists.size() - 1);
+    }
+};
+
+// 链表加法
+class Solution {
+public:
+    ListNode* addInList(ListNode* head1, ListNode* head2) {
+        stack<ListNode*> st1;
+        stack<ListNode*> st2;
+        while (head1) {
+            st1.push(head1);
+            head1 = head1->next;
+        }
+        while (head2) {
+            st2.push(head2);
+            head2 = head2->next;
+        }
+        ListNode* res = NULL;
+        int cnt = 0;
+        while (cnt || st1.size() || st2.size()) {
+            int a = 0, b = 0;
+            if (st1.size()) {
+                ListNode* temp = st1.top();
+                st1.pop();
+                a = temp->val;
+            }
+            if (st2.size()) {
+                ListNode* temp = st2.top();
+                st2.pop();
+                b = temp->val;
+            }
+            int val = (a + b + cnt) % 10;
+            cnt = (a + b + cnt) / 10;
+            ListNode* p = new ListNode(val);
+            if (res == NULL) res = p;
+            else {
+                p->next = res;
+                res = p;
+            }
+        }
+        return res;
     }
 };
